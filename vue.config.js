fdf30f3401216c857ed425450bf64d4d2522583e
likaorou <774897985@qq.com>
2022-07-28 22:02:08 +0800

    加载json

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,29 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   devServer: {
     port: 8080,
+    proxy: {
+      'api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^api': ''
+        }
+      }
+    }
+  },
+  configureWebpack: {
+    performance: {
+      hints: 'warning',
+      // 入口起点的最大体积
+      maxEntrypointSize: 50000000,
+      // 生成文件的最大体积
+      maxAssetSize: 30000000,
+      // 只给出 js 文件的性能提示
+      assetFilter: function (assetFilename) {
+        return assetFilename.endsWith('.js')
+      }
+    }
   },
   publicPath: './',
   outputDir: 'dist',
